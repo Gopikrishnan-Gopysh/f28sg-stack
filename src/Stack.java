649105f592ca87f4d0ed1f78ffdd2872e40fccc4
Rob Stewart <robstewart57@gmail.com>
2020-08-29 11:18:36 +0100

    Initial commit

---
+++ src/Stack.java
@@ -0,0 +1,82 @@
+class StackException extends RuntimeException{    
+	public StackException(String err) {
+		super(err);
+	}
+}
+
+public class Stack {
+	private Object[] S;
+	private int top;
+	private int capacity;
+	
+	private static int DEFAULT_SIZE = 100;
+	
+	public Stack(int size){
+		capacity = size;
+		S = new Object[size];
+		top = -1;
+	}
+
+	public Stack(){
+		this(DEFAULT_SIZE);
+	}
+	
+	/**
+	 * @return true if there are no elements in the stack
+	 */
+	public boolean isEmpty(){
+		// TODO
+		return false;
+	}
+	
+	/**
+	 * @return a count of the number of elements in the stack
+	 */
+	public int size(){
+		// TODO
+		return -1;
+	}
+	
+
+	/** Adds a new element to the stack
+	 * 
+	 * @param e the object to add to the top of the stack
+	 */
+	public void push(Object e){
+		// TODO
+	}
+	
+	/** Removes an element from the top of the stack
+	 * 
+	 * @return the object that was at the top of the stack
+	 * @throws StackException if the stack is empty
+	 */
+	public Object pop() throws StackException{
+		// TODO
+		return null;
+	}
+	
+	/** Returns object at the top of the stack
+	 * 
+	 * @return the object at the top of the stack
+	 * @throws StackException is the stack is empty
+	 */
+	public Object top() throws StackException{
+		// TODO
+		return null;
+	}
+	
+	/** A helper method to pretty-print the elements in the stack
+	 *
+	 */
+	public String toString(){
+		StringBuffer buf = new StringBuffer("[");
+		if(size() > 0)
+			buf.append(S[0]);
+		for(int i = 1; i <= top;i++){
+			buf.append(", " + S[i]);
+		}
+		buf.append("]");
+		return buf.toString();
+	}
+}
